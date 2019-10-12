2110adc494b0f4871e4f05543e94caa687f2147f
Richard Smith <rsmith37@ggc.edu>
2019-10-12 13:32:34 -0400

    add GET to events

--- routes/api/events.js
+++ routes/api/events.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
+const momemnt = require('moment');
 
 // Event input validation
 const validateEventInput = require('../../validation/event');
@@ -13,6 +14,34 @@ const Event = require('../../models/Event');
 // @desc    Tests events route
 // @access  Public
 router.get('/test', (req, res) => res.json({msg: "Events Works"}));
+
+// @route   GET api/events
+// @desc    Get events
+// @access  Public
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { errors, isValid } = validateEventInput(req.body);
+
+  const startDT = momemnt(req.body.start).startOf('day').toDate();
+  const endDT = momemnt(req.body.start).endOf('day').toDate();
+  console.log(startDT);
+  console.log(endDT);
+
+  Event.find({
+    start: {
+      $gte: startDT,
+      $lte: endDT
+    }
+  })
+    .then(event => {
+      if (!event) {
+        errors.event = "There are no events for this start date";
+        return res.status(404).json(errors);
+      }
+
+      res.json(event);
+    })
+    .catch(err => res.status(404).json(err));
+})
 
 // @route   POST api/events
 // @desc    Create event
